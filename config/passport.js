e2c291bbb28a1e2385d0704c234661450b7fca3c
naman0601 <73053395+naman0601@users.noreply.github.com>
2022-09-05 14:07:44 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,41 @@
-const LocalStrat = require('passport-local').Strategy;
-const mongoose = require('mongoose');
+const LocalStrat = require("passport-local").Strategy;
+const mongoose = require("mongoose");
 
 //Load User MOdel
-const User = require('../models/User');
+const User = require("../models/User");
 
-module.exports = function(passport){
-    passport.use(
-        new LocalStrat({ usernameField: 'EId'},(EId, password,done)=>{
-            //Match User
-            User.findOne({EId:EId})
-            .then(user=>{
-                if(!user){
-                    return done(null,false,{message: 'Employee ID is not registered'});
-                }
-                //Match Password
-                if(password===user.password)
-                        {return done(null,user);}
-                else{
-                    return done(null,false,{message : 'Password is Incorrect'});
-                }
+module.exports = async function (passport) {
+  passport.use(
+    new LocalStrat({ usernameField: "UserId" }, async (UserId, password, done) => {
+      //Match User
+      console.log(UserId, password);
+    //   const find = await User.find({ UserId: UserId }).then((user) => {
+    //     console.log(user);
+    //   });
+      await User.findOne({ UserId: UserId }).then((user) => {
+        // console.log(user);
+        if (!user) {
+          return done(null, false, {
+            message: "Employee ID is not registered",
+          });
+        }
+        //Match Password
+        console.log('Here',user);
+        if (password === user.password) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Password is Incorrect" });
+        }
+      });
+    })
+  );
 
-                });
-
-        })
-    );
-
-        passport.serializeUser(function(user,done){
-            done(null,user.id);
-        });
-        passport.deserializeUser((id,done)=>{
-            User.findById(id,(err,user)=>{
-                done(err,user);
-            })
-        })
-}
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
